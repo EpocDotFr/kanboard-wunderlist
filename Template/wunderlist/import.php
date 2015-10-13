006f0b6d5fca1fe8174a9b4c4498af16d79522ef
Epoc <contact@epoc.fr>
2015-10-13 23:54:17 +0200

    Useless

--- Template/wunderlist/import.php
+++ Template/wunderlist/import.php
@@ -4,6 +4,7 @@
 
 <form action="<?= $this->url->href('file', 'save') ?>" method="post" enctype="multipart/form-data">
   <?= $this->form->csrf() ?>
+  <label><?= t('Please choose the Wunderlist export file (*.json).') ?></label>
   <input type="file" name="files[]" multiple />
   <div class="form-help"><?= t('Maximum size: ') ?><?= is_integer($max_size) ? $this->text->bytes($max_size) : $max_size ?></div>
   <div class="form-actions">
