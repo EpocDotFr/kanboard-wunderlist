55a7068cd4a08ee1b0b9638065eaebdffd3a1d34
Dmitry <kochevd@gmail.com>
2016-10-08 20:52:04 +0300

    Create translations.php

---
+++ Locale/ru_RU/translations.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+  'Allow you to import Wunderlist tasks and lists by uploading an export file' => 'Позволит вам импортировать задачи и списки из Wunderlist, загрузив файл экспорта',
+  'Import from Wunderlist' => 'Импортировать из Wunderlist',
+  'Import' => 'Импортировать',
+  'Please choose the Wunderlist export file (usually wunderlist-*.json) :' => 'Пожалуйста, выберите файл экспорта Wunderlist (обычно wunderlist-*.json) :',
+  'Please select a file' => 'Пожалуйста, выберите файл',
+  'An error occured while uploading the Wunderlist export file' => 'Произошла ошибка во время загрузки файла экспорта Wunderlist',
+  'Error reading the Wunderlist export file' => 'Произошла ошибка при чтении файла экспорта Wunderlist',
+  'Error reading the JSON data from the Wunderlist export file' => 'Произошла ошибка при чтении JSON из файла экспорта Wunderlist',
+  'Wunderlist file imported successfuly' => 'Импорт файла из Wunderlist успешно завершен',
+  'An error occured while importing the project %s' => 'Произошла ошибка при импорте проекта %s',
+  'An error occured while importing the task %s' => 'Произошла ошибка при импорте задачи %s',
+  'An error occured while importing the subtask %s' => 'Произошла ошибка при импорте подзадачи %s',
+);
